7131b2448bd32446906e3be721be5c643a5d25c2
snez333 <sineziwe03@gmail.com>
2024-06-01 23:13:23 +0200

    signup

--- app/src/main/java/com/example/myapplication/MoodActivity.java
+++ app/src/main/java/com/example/myapplication/MoodActivity.java
@@ -4,8 +4,12 @@ import static android.content.ContentValues.TAG;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CalendarView;
 
 import androidx.activity.EdgeToEdge;
@@ -16,9 +20,15 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MoodActivity extends AppCompatActivity {
-private CalendarView cal;
+    private CalendarView cal;
+    private Button buttonHappy, buttonAnxious, buttonSad, buttonUpset;
+    private Map<String, String> dateMoodMap;
+    private String selectedDate;
+
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,17 +36,54 @@ private CalendarView cal;
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_mood);
         cal = (CalendarView) findViewById(R.id.calendarView);
+        buttonHappy = findViewById(R.id.button_happy);
+        buttonAnxious = findViewById(R.id.button_anxious);
+        buttonSad = findViewById(R.id.button_sad);
+        buttonUpset = findViewById(R.id.button_upset);
+
+
+        dateMoodMap = new HashMap<>();
         cal.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(@NonNull CalendarView view, int i, int i1, int i2) {
-                String date = i2 + "/" + (i1 +1) + "/" + i;
-                Log.d(TAG, "onSelectedDayChange: dd/mm/yyyy:" + date);
+                String selectedDate = i2 + "/" + (i1 + 1) + "/" + i;
+//            Log.d(TAG, "onSelectedDayChange: dd/mm/yyyy:" + date);
 
-                Intent intent = new Intent(MoodActivity.this,Mood2Activity.class);
-                intent.putExtra("date", date);
-                startActivity(intent);
+//                Intent intent = new Intent(MoodActivity.this,Mood2Activity.class);
+//                intent.putExtra("date", date);
+//                startActivity(intent);
             }
-    });
+        });
+
+        buttonHappy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        buttonAnxious.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+
+
+        });
+
+        buttonSad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        buttonUpset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
     }
 }
+
+
